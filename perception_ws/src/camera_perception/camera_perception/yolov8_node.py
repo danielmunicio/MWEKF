81f23ff8c5eb4268a4ec4730ad77807567f278ec
Leonid Pototskiy <pototskiy.leonid@gmail.com>
2023-11-06 19:57:48 -0800

    Created msg package and minimal working yolo publisher

--- perception_ws/src/camera_perception/camera_perception/yolov8_node.py
+++ perception_ws/src/camera_perception/camera_perception/yolov8_node.py
@@ -5,30 +5,70 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
 
-bridge = CvBridge()
+from feb_msgs.msg import YoloInference, InferenceResult
 
 class YoloNode(Node):
     def __init__(self):
         super().__init__('yolo_node')
         
+        # SUBSCRIBERS
         self.camera_sub = self.create_subscription(
             Image,
             '/color/image_raw',
             self.camera_callback,
             10)
-        self.camera_sub
         
-        self.model = YOLO('yolov8n.pt')
+        # PUBLISHERS
+        self.yolo_pub = self.create_publisher(YoloInference, '/perception/yolo_inference', 1)
+        self.img_pub = self.create_publisher(Image, '/perception/inference_result', 1)
+        
+        # GLOBAL VARIABLES
+        self.model = YOLO('yolov8m-seg.pt')
         self.model.fuse()
+        self.bridge = CvBridge()
+        self.inference = YoloInference()
+        
         
     
     def camera_callback(self, msg: Image):
-        img = bridge.imgmsg_to_cv2(msg, "bgr8")
+        img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        width, height = msg.width, msg.height
         results = self.model(img)
+        
+        # Create inference message header
+        self.inference.header.frame_id = "camera_color_optical_frame"
+        self.inference.header.stamp = self.get_clock().now().to_msg()
+        
+        """
+        for r in results:
+            for mask in r.masks:
+                # scale for visualizing results
+                msk = mask.data.numpy() * 255
+                print(msk.shape)
+        """
+                
+        
+        img_msg = self.bridge.cv2_to_imgmsg(results[0].plot())
+        #print(img_msg.width, img_msg.height)
+        self.img_pub.publish(img_msg)
+        
+        self.yolo_pub.publish(self.inference)
+        self.inference.yolo_inference.clear()
+        
+        
+        
+        """
+        annotated_frame = results[0].plot()
+        img_msg = self.bridge.cv2_to_imgmsg(annotated_frame)  
+
+        self.img_pub.publish(img_msg)
+        self.yolo_pub.publish(self.inference)
+        self.inference.yolo_inference.clear()
+        """
         
 
 def main():
     rclpy.init(args=None)
-    camera_subscriber = YoloNode()
-    rclpy.spin(camera_subscriber)
+    yolo_node = YoloNode()
+    rclpy.spin(yolo_node)
     rclpy.shutdown()

389a1156e52371c9ad8b2dde67eba8ebdd23381a
Leonid Pototskiy <pototskiy.leonid@gmail.com>
2023-11-10 16:44:57 -0800

    Working communications workspace and YOLO node

--- perception_ws/src/camera_perception/camera_perception/yolov8_node.py
+++ perception_ws/src/camera_perception/camera_perception/yolov8_node.py
@@ -1,11 +1,13 @@
 from ultralytics import YOLO
-import rclpy
 from rclpy.node import Node
+from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
-import cv2
+import rclpy
+import os
+import time
 
-from feb_msgs.msg import YoloInference, InferenceResult
+from feb_msgs.msg import Bitmasks, MatrixInt
 
 class YoloNode(Node):
     def __init__(self):
@@ -19,52 +21,67 @@ class YoloNode(Node):
             10)
         
         # PUBLISHERS
-        self.yolo_pub = self.create_publisher(YoloInference, '/perception/yolo_inference', 1)
-        self.img_pub = self.create_publisher(Image, '/perception/inference_result', 1)
+        self.img_pub = self.create_publisher(Image, '/perception/mask_debug', 1)
+        self.masks_publisher = self.create_publisher(Bitmasks, 'perception/masks', 1)
+        
         
         # GLOBAL VARIABLES
-        self.model = YOLO('yolov8m-seg.pt')
+        current_path = os.path.dirname(__file__)
+        root = current_path.partition('perception_ws')[0]
+        # Find the absolute path to the model
+        filename = os.path.join(root, 'perception_ws/src/camera_perception/best.pt')
+        self.model = YOLO(filename)
         self.model.fuse()
-        self.bridge = CvBridge()
-        self.inference = YoloInference()
-        
+        self.bridge = CvBridge()        
         
     
     def camera_callback(self, msg: Image):
-        img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-        width, height = msg.width, msg.height
-        results = self.model(img)
+        # Convert message to CV2 image
+        img = self.bridge.imgmsg_to_cv2(msg, 'passthrough')
+        # Do segmentation
+        start = time.perf_counter()
+        results = self.model(img, verbose=False)
+        end = time.perf_counter()
+        seg_time = end - start
         
-        # Create inference message header
-        self.inference.header.frame_id = "camera_color_optical_frame"
-        self.inference.header.stamp = self.get_clock().now().to_msg()
+        # Extract bitmasks (THIS IS SLOW!!)
+        start = time.perf_counter()
+        mask_list = []
+        for mask in results[0].masks:
+            # Convert the mask to numpy 2D array
+            matrix = mask.data.numpy()[0]
+            # Create matrix message
+            matrix_msg = MatrixInt()
+            matrix_msg.rows, matrix_msg.cols = matrix.shape
+            matrix_msg.entries = matrix.flatten().astype(int).tolist() # Very slow, needs optimizing
+            # Append to list of masks
+            mask_list.append(matrix_msg)
         
-        """
-        for r in results:
-            for mask in r.masks:
-                # scale for visualizing results
-                msk = mask.data.numpy() * 255
-                print(msk.shape)
-        """
-                
+        # Extract colors
+        colors = list(results[0].names.values())
+        color_list = []
+        for c in colors:
+            color_string = String()
+            color_string.data = c
+            color_list.append(color_string)
         
+        
+        # Create bitmask list message
+        mask_list_msg = Bitmasks()
+        mask_list_msg.bitmask_list = mask_list
+        mask_list_msg.colors = color_list
+        mask_list_msg.header.frame_id = 'camera_color_optical_frame'
+        mask_list_msg.header.stamp = self.get_clock().now().to_msg()
+        self.masks_publisher.publish(mask_list_msg)
+        end = time.perf_counter()
+        post_process_time = end - start
+        
+        # Publish debugging image
         img_msg = self.bridge.cv2_to_imgmsg(results[0].plot())
-        #print(img_msg.width, img_msg.height)
         self.img_pub.publish(img_msg)
         
-        self.yolo_pub.publish(self.inference)
-        self.inference.yolo_inference.clear()
-        
-        
-        
-        """
-        annotated_frame = results[0].plot()
-        img_msg = self.bridge.cv2_to_imgmsg(annotated_frame)  
+        self.get_logger().info(f"Segmentation time: {'%.4f' % seg_time} s.\tPostprocessing time: {'%.4f' % post_process_time} s.")
 
-        self.img_pub.publish(img_msg)
-        self.yolo_pub.publish(self.inference)
-        self.inference.yolo_inference.clear()
-        """
         
 
 def main():

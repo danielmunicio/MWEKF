3d42aec5a6feb6a7124af40413f92956a4762a30
Sricharan Guddanti <sriguddanti@gmail.com>
2024-02-24 10:03:19 -0800

    ckpt, added all mpc code

--- pnc_ws/mpc/mpc/mpc.py
+++ pnc_ws/mpc/mpc/mpc.py
@@ -1,6 +1,19 @@
-def main():
-    print('Hi from mpc.')
+import rclpy
+from rclpy.node import Node
 
+def main(args=None):
+    rclpy.init(args=args)
+        
+    # node = LidarBoardDetector()
+    node = CameraProjection()
+
+    rclpy.spin(node)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    node.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()

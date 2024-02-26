df240461e7fa14f382c10b2bbd332195a3d63e2b
Arul Loomba <Arul>
2024-02-26 14:20:09 -0800

    main.py almost done

--- pnc_ws/global_path/global_path/main.py
+++ pnc_ws/global_path/global_path/main.py
@@ -3,6 +3,7 @@ from global_opt_settings import settings as settings
 from global_opt_compiled import CompiledGlobalOpt
 from rclpy.node import Node
 import numpy as np
+from global_opt_settings import settings
 from feb_msgs import State
 from feb_msgs import PathState
 
@@ -19,13 +20,20 @@ class GlobalPath(Node):
 
         ## PSUEDOCODE
         # input: cones from slam
-        g = CompiledGlobalOpt()
-        left_points, right_points = yashs_algorithm(bluecones, yellowcones)
+        blue_cones = ... #Message from /slam/matched/global
+        yellow_cones = ... #Message from /slam/matched/global
+        g = CompiledGlobalOpt(**settings)
+        
+        g.construct_solver()
+        g.load_solver()
+
+        left_points, right_points = yashs_algorithm(blue_cones, yellow_cones)
         res = g.solve(left_points, right_points)
         states, controls = g.to_constant_tgrid(**res)
+        bools = []
+        #for i in states, determine whether each state is true or false and add it to bools
+        #publish bools to /global/finished
+    
+        
 
-<<<<<<< HEAD
         publish(states)
-=======
-        publish(states)
->>>>>>> 1655b97 (added psuedocode)

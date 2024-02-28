06d5756aa302e50973137fd88eaf2ed00270a8b4
reid23 <reid.dye@gmail.com>
2024-02-27 18:47:04 -0800

    made build script and wrote settings metaclass. settings are much cleaner now

--- pnc_ws/global_path/global_path/build_script.py
+++ pnc_ws/global_path/global_path/build_script.py
@@ -1,4 +1,6 @@
 from global_opt_compiled import CompiledGlobalOpt
-from global_opt_settings import settings
-print(dir(settings))
-# g = CompiledGlobalOpt()
+from global_opt_settings import GlobalOptSettings
+
+if __name__ == '__main__':
+    g = CompiledGlobalOpt(**GlobalOptSettings)
+    g.construct_solver(generate_c=True, compile_c=True)

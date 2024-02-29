9b3a70342f842e21c2f3af31d3e14bdcd41ddb58
reid23 <reid.dye@gmail.com>
2024-02-29 11:10:15 -0800

    added if name==main

--- pnc_ws/global_path/global_path/build_script.py
+++ pnc_ws/global_path/global_path/build_script.py
@@ -1,6 +1,9 @@
 from global_opt_compiled import CompiledGlobalOpt
 from global_opt_settings import GlobalOptSettings
 
-if __name__ == '__main__':
+def build_solver():
     g = CompiledGlobalOpt(**GlobalOptSettings)
     g.construct_solver(generate_c=True, compile_c=True)
+
+if __name__ == '__main__':
+    build_solver()

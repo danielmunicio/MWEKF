daf71f1029fceeb995882f7cba6bd41f5854c315
timothyypark <timmypark@berkeley.edu>
2024-03-11 07:35:48 +0000

    first iteration of update state callback function for SLAM

--- slam_ws/src/graphslam-global/graphslam-global/graphslam-global.py
+++ slam_ws/src/graphslam-global/graphslam-global/graphslam-global.py
@@ -11,9 +11,9 @@ import math
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64, Header
 from sensor_msgs.msg import Imu
-
+from geometry_msgs.msg import Quaternion, Vector3
 
 from feb_msgs.msg import State
 from feb_msgs.msg import FebPath
@@ -64,21 +64,124 @@ class GraphSLAM_Global(Node):
         # Initializes a new instance of graphslam from the graphslam
         # Data Association Threshold is to be tweaked
         self.slam = slamLib.GraphSLAM(solver_type='qp', landmarkTolerance=4)
+        
+        # used to calculate the state of the vehicle
+        self.statetimestamp = 0.0
+        self.currentstate = State()
     
     
+
+    """
+    Function that takes in message header and computes difference in time from last state msg
+    Input: Header (std_msg/Header)
+    - uint32 seq
+    - time stamp
+    - string frame_id
+    Output: timediff: float
+    """
+    def compute_timediff(self, header: Header) -> float:
+        newtime = header.stamp.sec + 1e-9 * header.stamp.nanosec
+        timediff = newtime - self.statetimestamp
+        self.statetimestamp = newtime
+
+        timediff = np.round(timediff, 4)
+
+        return timediff
+    
+    """
+    Function that takes in quaternion and converts to Eulerian angles
+    Input: Quat (Quaternion)
+    - float x
+    - float y
+    - float z
+    - float w
+    Output: roll, pitch, yaw
+    #NOTE: roll and pitch are irrelevant as of now, we only care about heading angle (pitch)
+    """
+
+    def quat_to_euler(self, quat: Quaternion) -> tuple[float, float, float]:
+        x = quat.x
+        y = quat.y
+        z = quat.z
+        w = quat.w
+
+        roll = math.atan2(2.0 * (w * x + y * z), 1.0 - 2.0 * (x * x + y * x))
+        pitch = math.asin(2.0 * (w * y - z * x))
+        yaw = math.atan2(2.0 * (w * z + x * y), 1.0 - 2.0 * (y * y + z * z))
+
+        return roll, pitch, yaw
+
+    """
+    Function that takes in linear acceleration and dt and outputs velocity (linear)
+    Input:
+    - linear_acceleration: from imu message
+    - dt: calculated at each time step
+    Output:
+    - linear_velocity
+    #NOTE: we assume linear acceleration is in the car's frame. This means
+            x is in the longitudinal direction (positive = forwards)
+            y is in the lateral direction (positive = to the right)
+            depending on how IMU is providing this data, change accordingly
+    """
+    def compute_velocity(self, acc: Vector3, dt: float) -> float:
+        longitudinal_acc = acc.x
+        # lateral_acc = acc.y # May be needed in the future if  
+        #                       straightline model is not accurate enough
+
+        linear_velocity = longitudinal_acc * dt
+        return linear_velocity
+
+    """
+    Function that updates the State.msg variables after a new state has been produced
+    Inputs: 
+    - dx: change in position [delta_x, delta_y]
+    - yaw: new heading (theta)
+    - velocity
+    Outputs: None
+    """
+    def update_state(self, dx: np.array, yaw: float, velocity: float) -> None:
+        self.currentstate.carstate[0] += dx[0]
+        self.currentstate.carstate[1] += dx[1]
+        self.currentstate.carstate[2] = velocity
+        self.currentstate.carstate[3] = yaw
+
+
     """
     Function that takes in IMU messages and processes GraphSLAM based upon 
     Input: imu (Imu_msg)
     - geometry_msgs/Quarternion orientation
-        - float64[9] orientation_covariance
+    - float64[9] orientation_covariance
     - geometry_msgs/Vector3 angular_velocity
-        - float64[9] angular_velocity_covariance
+    - float64[9] angular_velocity_covariance
     - geometry_msgs/Vector3 linear_acceleration
-        - float64[9] linear_acceleration_covariance
+    - float64[9] linear_acceleration_covariance
     """
+
     def imu_callback(self, imu: Imu) -> None:
-        # Dummy Function for now, need to decode the values (covariances)
         pass
+        # process time
+        dt = self.compute_timediff(imu.header)
+        # generate current heading
+        roll, pitch, yaw = self.quat_to_euler(imu.orientation)
+        self.currentstate.heading = yaw
+        # generate current velocity
+        velocity = self.compute_velocity(imu.linear_acceleration, dt)
+        # for now, we assume velocity is in the direction of heading
+
+        self.currentstate.velocity = velocity
+
+        # generate dx [change in x, change in y] to add new pose to graph
+        dx = velocity * dt * np.array([math.cos(yaw), math.sin(yaw)])
+
+        # add new position node to graph
+        self.slam.update_position(dx)
+
+        # update state msg
+        self.update_state(dx, yaw, velocity)
+
+        self.state_pub.publish(self.currentstate)
+
+
 
     """
     Function that takes the list of cones, updates and solves the graph

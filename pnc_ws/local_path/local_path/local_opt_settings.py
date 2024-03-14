98b655b7e17bc003057a33441bcde970f293273d
Ashton Lee <ashton_lee@berkeley.edu>
2024-03-14 14:59:21 -0700

    moved and rename files to local_path folder

---
+++ pnc_ws/local_path/local_path/local_opt_settings.py
@@ -0,0 +1,16 @@
+from .Settings import Settings
+from casadi import Function
+    
+class LocalOptSettings(metaclass=Settings):
+    """settings for LocalOpt. All in one place, so it's always synced."""
+    N: int = 100
+    solver: str = 'ipopt'
+    car_params: dict[str:float] = {'l_r': 1.4987, 'l_f':1.5213, 'm': 1.}
+    bbox: dict[str:float] = {'l': 2, 'w': 1}
+    DF_MAX: float  =  0.5
+    ACC_MIN: float = -3.0
+    ACC_MAX_FN: float|Function = 2.0
+    DF_DOT_MAX: float =  0.5
+    V_MIN: float = 0.0
+    V_MAX: float = 25.0
+    FRIC_MAX: float|Function = 12.0

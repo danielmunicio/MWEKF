7c764bd55bedc1ab656b664742128d7833ae1474
timothyypark <timmypark@berkeley.edu>
2024-03-26 12:09:43 -0500

    started sims section for slam

--- slam_ws/src/graphslam-global/setup.py
+++ slam_ws/src/graphslam-global/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'graphslam-global = graphslam-global.graphslam-global:main'
         ],
     },
 )

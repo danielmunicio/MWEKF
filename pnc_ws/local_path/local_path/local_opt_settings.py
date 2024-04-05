c7d931ac4d507e8cefa6e8ed1fbeb985d3f9eca2
reid23 <reid.dye@gmail.com>
2024-04-05 14:42:09 -0700

    changed car parameters for path planning. still need to do mpc

--- pnc_ws/local_path/local_path/local_opt_settings.py
+++ pnc_ws/local_path/local_path/local_opt_settings.py
@@ -5,8 +5,9 @@ class LocalOptSettings(metaclass=Settings):
     """settings for LocalOpt. All in one place, so it's always synced."""
     N: int = 10 # for testing, input 10 pairs of cones to test
     solver: str = 'ipopt'
-    car_params: dict[str:float] = {'l_r': 1.4987, 'l_f':1.5213, 'm': 1.}
-    bbox: dict[str:float] = {'l': 2, 'w': 1}
+    # car_params: dict[str:float] = {'l_r': 1.4987, 'l_f':1.5213, 'm': 1.}1.201
+    car_params: dict[str:float] = {'l_r': 0.76, 'l_f':0.76, 'm': 1.}
+    bbox: dict[str:float] = {'l': 2.7, 'w': 1.6}
     DF_MAX: float  =  0.5
     ACC_MIN: float = -3.0
     ACC_MAX_FN: float|Function = 2.0

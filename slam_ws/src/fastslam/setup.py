31d2d31e673a82f0b0efb4f86753f6b30f9b90b9
reid23 <reid.dye@gmail.com>
2024-04-07 19:49:09 -0700

    launcher and slam fixes

--- slam_ws/src/fastslam/setup.py
+++ slam_ws/src/fastslam/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'fastslam = fastslam.fastslam:fastslam'
+            'fastslam = fastslam.fastslam:main'
         ],
     },
 )
---
+++ starter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source install/setup.sh
+ros2 run fastslam fastslam
+ros2 run graphslam_global graphslam_global
+ros2 run global_path global_path
+ros2 run local_path local_path
+ros2 run mpc mpc

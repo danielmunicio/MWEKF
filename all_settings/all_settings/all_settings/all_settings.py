006ba49506137a7ab01994dcc968b716b547a7b7
Jetson <febautonomous@gmail.com>
2024-07-21 20:53:52 -0700

    low level control worksgit add .git add .git add .!

--- all_settings/all_settings/all_settings/all_settings.py
+++ all_settings/all_settings/all_settings/all_settings.py
@@ -6,15 +6,15 @@ class GlobalOptSettings(metaclass=Settings):
     """settings for CompiledGlobalOpt. All in one place, so it's always synced."""
     N: int = 38
     solver: str = 'ipopt'
-    car_params: dict[str:float] = {'l_r': 0.76, 'l_f':0.76, 'm': 1.}
-    bbox: dict[str:float] = {'l': 2.7+1, 'w': 1.6+1}
+    car_params: dict = {'l_r': 0.76, 'l_f':0.76, 'm': 1.}
+    bbox: dict = {'l': 2.7+1, 'w': 1.6+1}
     DF_MAX: float  =  0.5
     ACC_MIN: float = -3.0
-    ACC_MAX_FN: float|Function = 2.0
+    ACC_MAX_FN = 2.0 # float or function
     DF_DOT_MAX: float =  0.5
     V_MIN: float = 0.0
     V_MAX: float = 25.0
-    FRIC_MAX: float|Function = 12.0
+    FRIC_MAX = 12.0 # float or function
 
 
 class LocalOptSettings(metaclass=Settings):
@@ -22,15 +22,15 @@ class LocalOptSettings(metaclass=Settings):
     N: int = 10 # for testing, input 10 pairs of cones to test
     solver: str = 'ipopt'
     # car_params: dict[str:float] = {'l_r': 1.4987, 'l_f':1.5213, 'm': 1.}1.201
-    car_params: dict[str:float] = {'l_r': 0.76, 'l_f':0.76, 'm': 1.}
-    bbox: dict[str:float] = {'l': 2.7, 'w': 1.6}
+    car_params: dict = {'l_r': 0.76, 'l_f':0.76, 'm': 1.}
+    bbox: dict = {'l': 2.7, 'w': 1.6}
     DF_MAX: float  =  0.5
     ACC_MIN: float = -3.0
-    ACC_MAX_FN: float|Function = 2.0
+    ACC_MAX_FN = 2.0 # float or function
     DF_DOT_MAX: float =  0.5
     V_MIN: float = 0.0
     V_MAX: float = 10.0
-    FRIC_MAX: float|Function = 12.0
+    FRIC_MAX = 12.0 # float or function
 
 
 class MPCSettings(metaclass=Settings):
@@ -50,10 +50,10 @@ class MPCSettings(metaclass=Settings):
     DF_DOT_MIN: float = -0.5
     DF_DOT_MAX: float =  0.5
     # Q: list[float] = [1., 1., 10., 0.1]
-    Q: list[float] = [5., 5., 0., 0.]
-    R: list[float] = [10., 100.]
+    Q: list = [5., 5., 0., 0.]
+    R: list = [10., 100.]
     # R: list[float] = [0., 0.]
-    F: list[float] = [0., 0., 0., 0.]
+    F: list = [0., 0., 0., 0.]
     TRACK_SLACK_WEIGHT: float = 5e5
     use_rk_dynamics: bool = False
     solver: str = 'ipopt'
@@ -65,12 +65,12 @@ class CANSettings(metaclass=Settings):
 
 class SteeringSettings(metaclass=Settings):
     MOTOR_TO_WHEELS_RATIO: float = 1.0 # amount motor spins/amount wheels turn
-    MAX_MOTOR_SPEED: float = 1.0 # positive, radians/second
+    MAX_MOTOR_SPEED: float = 10.0 # positive, radians/second
     MOTOR_TICKS_PER_RAD: float = 18000/pi # ticks of motor encoder per radian of motor turn
     CAN_SETTINGS = CANSettings
 
 class BBWSerialSettings(metaclass=Settings):
-    port: str = '/dev/serial/by-id/{whatever idk}'
+    port: str = '/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_75033303634351C041A1-if00'
     baudrate: int = 115200
     timeout: float = 0.05
 

3c1cb63ae9b2a39be418d5d6defec43ce0b153e7
reid23 <reid.dye@gmail.com>
2024-07-22 22:30:28 -0700

    small updates to low level code and settings

--- all_settings/all_settings/all_settings/all_settings.py
+++ all_settings/all_settings/all_settings/all_settings.py
@@ -1,6 +1,7 @@
 from .Settings import Settings
 from casadi import Function
 from numpy import pi
+from typing import Union
 
 class GlobalOptSettings(metaclass=Settings):
     """settings for CompiledGlobalOpt. All in one place, so it's always synced."""
@@ -10,7 +11,7 @@ class GlobalOptSettings(metaclass=Settings):
     bbox: dict = {'l': 2.7+1, 'w': 1.6+1}
     DF_MAX: float  =  0.5
     ACC_MIN: float = -3.0
-    ACC_MAX_FN = 2.0 # float or function
+    ACC_MAX_FN: Union[float, Function] = 2.0
     DF_DOT_MAX: float =  0.5
     V_MIN: float = 0.0
     V_MAX: float = 25.0
@@ -21,16 +22,27 @@ class LocalOptSettings(metaclass=Settings):
     """settings for LocalOpt. All in one place, so it's always synced."""
     N: int = 10 # for testing, input 10 pairs of cones to test
     solver: str = 'ipopt'
+    solver_opts: dict[str:any] = {
+        # set linear solver
+        'ipopt.linear_solver': 'ma57',
+        # expand MX expressions to SX
+        'expand': True,
+        # make ipopt shut up
+        'ipopt.print_level': 0,
+        'ipopt.sb': 'yes',
+        'ipopt.ma57_automatic_scaling': 'no',
+        'print_time': 0,
+    }
     # car_params: dict[str:float] = {'l_r': 1.4987, 'l_f':1.5213, 'm': 1.}1.201
     car_params: dict = {'l_r': 0.76, 'l_f':0.76, 'm': 1.}
     bbox: dict = {'l': 2.7, 'w': 1.6}
     DF_MAX: float  =  0.5
     ACC_MIN: float = -3.0
-    ACC_MAX_FN = 2.0 # float or function
+    ACC_MAX_FN: Union[float, Function] = 2.0
     DF_DOT_MAX: float =  0.5
     V_MIN: float = 0.0
     V_MAX: float = 10.0
-    FRIC_MAX = 12.0 # float or function
+    FRIC_MAX: Union[float, Function] = 12.0
 
 
 class MPCSettings(metaclass=Settings):
@@ -64,10 +76,10 @@ class CANSettings(metaclass=Settings):
     bitrate: int = 1000000
 
 class SteeringSettings(metaclass=Settings):
+    CAN_SETTINGS: Settings = CANSettings
     MOTOR_TO_WHEELS_RATIO: float = 1.0 # amount motor spins/amount wheels turn
     MAX_MOTOR_SPEED: float = 10.0 # positive, radians/second
     MOTOR_TICKS_PER_RAD: float = 18000/pi # ticks of motor encoder per radian of motor turn
-    CAN_SETTINGS = CANSettings
 
 class BBWSerialSettings(metaclass=Settings):
     port: str = '/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_75033303634351C041A1-if00'
@@ -75,6 +87,7 @@ class BBWSerialSettings(metaclass=Settings):
     timeout: float = 0.05
 
 class BrakingSettings(metaclass=Settings):
-    SERIAL_SETTINGS = BBWSerialSettings
-    PSI_PER_VOLT = 25.0
-    VMAX = 4.7 # max voltage the arduino can output (at PWM duty cycle 255)
+    SERIAL_SETTINGS: Settings = BBWSerialSettings
+    VOLTS_PER_PSI: float = 0.0271494 # computed using linear regression on desmos with like 3 datapoints
+    VOLTS_FOR_ZERO_PSI: float = 0.962366 # same regression ^
+    VMAX: float = 4.7 # max voltage the arduino can output (at PWM duty cycle 255)

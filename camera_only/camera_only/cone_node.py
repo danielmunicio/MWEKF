d9c35962138c6961294729da63e00b72aed207f7
febauto <febautonomous@gmail.com>
2024-08-14 16:49:05 -0700

    uuuuuuhhh, yeah you can push those (- daniel)

--- camera_only/camera_only/cone_node.py
+++ camera_only/camera_only/cone_node.py
@@ -5,7 +5,6 @@ from std_msgs.msg import Header
 from std_msgs.msg import Float64
 from feb_msgs.msg import Cones
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
 import rclpy
 import os, glob
 import time
@@ -15,6 +14,7 @@ import numpy as np
 import imutils
 import cv2
 from camera_only import cone_distance as cd
+from .NotCvBridge import imgmsg_to_cv2
 import sys
 sys.path.append('.')
 
@@ -31,37 +31,19 @@ class ConeNode(Node):
         )
 
         self.cone_pub = self.create_publisher(Cones,'/perception_cones',1)
-
- #       path_to_repo = current_path.partition('feb-system-integration')[0]
- #       model_dir = os.path.join(path_to_repo, 'feb-system-integration/camera_only_ws/src/camera_only/camera_only')
-   #     print("path to repo: ", path_to_repo)
-  #      print("model_dir: ", model_dir)
- #       model_files = [file for file in os.listdir(model_dir) if os.path.splitext(file)[1] == '.pt']
-  #      print("model files: ", model_files)
-   #     try:
-    #        model_path = os.path.join(model_dir, model_files[0])
-     #       if len(model_files) > 1:
-      #          self.get_logger().warn(f"Multiple YOLO models found in '{model_dir}', currently using '{model_files[0]}'")
-       # except IndexError:
-        #    self.get_logger().error(f"No YOLO model with .pt extension found in '{model_dir}', node shut down")
-         #   raise SystemExit
-        
-
         self.model = YOLO('bestest.pt')
         self.model.fuse()
-        self.bridge = CvBridge()
 
     def camera_callback(self, msg: Image):
         # Convert Image msg to CV2 image
-        img = self.bridge.imgmsg_to_cv2(msg, 'passthrough')
+        img = imgmsg_to_cv2(msg, 'passthrough')
         
                 # Convert ROS Image message to OpenCV image
-        bridge = CvBridge()
-        cv_image = bridge.imgmsg_to_cv2(msg, "bgr8")
+        cv_image = imgmsg_to_cv2(msg, "bgr8")
         
         # Display the image
-        cv2.imshow("Image Window", cv_image)
-        cv2.waitKey(1)  # Refresh the window
+        #cv2.imshow("Image Window", cv_image)
+        #cv2.waitKey(1)  # Refresh the window
 
         start = time.perf_counter()
 

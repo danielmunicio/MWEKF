bef00efb05f0d3da67e9a5687a207fd1c9e6d825
danielmunicio <danielmunicio360@gmail.com>
2024-09-06 12:16:23 -0700

    Removing Things

--- starter.sh
+++ starter.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 source install/setup.bash;
 ros2 run local_path compile_solver;
-rm boobies.txt cones.txt local_path.txt mpc.txt out.txt path_values.txt;
 ros2 run fastslam fastslam > /dev/null &
 ros2 run mpc mpc &
 ros2 run global_path global_path &

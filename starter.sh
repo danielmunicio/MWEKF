6b79c1a164318e917d853472eeac08576b9e43b7
Yash Kodali <yash.kodali@berkeley.edu>
2025-01-26 20:51:30 -0800

    [yash]: adjusted stater.sh

--- starter.sh
+++ starter.sh
@@ -3,7 +3,7 @@ source install/setup.bash;
 ros2 run local_path compile_solver;
 ros2 run fastslam fastslam > /dev/null &
 ros2 run mpc mpc &
-ros2 run global_path global_path &
+# ros2 run global_path global_path &
 ros2 run local_path local_path &
 sleep 1;
 ros2 run graphslam_global graphslam_global_fast

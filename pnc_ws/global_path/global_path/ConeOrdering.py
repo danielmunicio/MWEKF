cb2a99950371a2cfe141be06ca5d46ac45757f4d
Yash Kodali <yash.kodali@berkeley.edu>
2025-01-26 19:52:08 -0800

    updated changes to list handling

--- pnc_ws/global_path/global_path/ConeOrdering.py
+++ pnc_ws/global_path/global_path/ConeOrdering.py
@@ -35,12 +35,12 @@ def ConeOrdering(msg: Map, state: list[float]):
     # convert to lists of lists instead of Point objects
     # then check if it needs to be reversed
 
-    leftN_points, rightN_points = list(map(lambda p: [p.x, p.y], leftN_points)), list(map(lambda p: [p.x, p.y], rightN_points))
+    leftN_points, rightN_points = list(map(lambda p: [p[0], p[1]], leftN_points)), list(map(lambda p: [p[0], p[1]], rightN_points))
     if (np.linalg.norm((np.array(leftN_points[0])+np.array(rightN_points[0]))/2 - np.array(state[0:2]))
       > np.linalg.norm((np.array(leftN_points[-1])+np.array(rightN_points[-1]))/2 - np.array(state[0:2]))):
         leftN_points, rightN_points = leftN_points[::-1], rightN_points[::-1]
 
-    leftN_points = leftN_points[::int(300/N)]
-    rightN_points = rightN_points[::int(300/N)]
+    leftN_points = leftN_points[::int(bigN/N)]
+    rightN_points = rightN_points[::int(bigN/N)]
 
     return leftN_points, rightN_points

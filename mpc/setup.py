1792a994c829ae24910a87e8a2e39b95301012e5
reid23 <reid.dye@gmail.com>
2025-02-11 12:39:39 -0800

    [reid] cleanup of commpiled files and stuff

--- mpc/setup.py
+++ mpc/setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='reid',
-    maintainer_email='reid.dye@xz.ax',
+    maintainer_email='reid@xz.ax',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],

f0647ea8e239d86ed9358ae05535f7fae411d312
Daniel Municio <danielmunicio360@gmail.com>
2025-04-23 01:35:25 -0700

    No more double jeopardy, cartesian only

--- camera_only/camera_only/find_cone_cluster.py
+++ camera_only/camera_only/find_cone_cluster.py
@@ -1,13 +1,8 @@
 import os
 import sys
-import threading
-import multiprocessing
-import ast
-import time 
 # External modules
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
 # ROS 2 modules
 import rclpy
@@ -109,14 +104,14 @@ class SensorFusion(Node):
     def get_dist_angle_classes(self, is_realsense=False):
         if self.velodyne_msg is None or (is_realsense and self.realsense_image_msg) is None or (not is_realsense and self.logitech_image_msg) is None:
             return
+        # Convert camera into LiDAR coordinate system
         R, T = FileOperations.get_extrinsic_parameters(UTILITIES_PATH, is_realsense)
         RT = np.hstack((R, T.T))
         h, w, _, camera_matrix, _ = FileOperations.get_intrinsic_parameters(UTILITIES_PATH, is_realsense)
         h, w = 480, 640
         camera_matrix = np.array(camera_matrix, dtype=np.float64).reshape(3, 3)
         P = camera_matrix @ RT
-        intensities = ros2_numpy.point_cloud2.point_cloud2_to_array(self.velodyne_msg)['intensity']
-        max_intensity = np.max(intensities)
+
         points3D = ros2_numpy.point_cloud2.point_cloud2_to_array(self.velodyne_msg)['xyz']
         points3D = np.array(points3D, dtype=np.float64)
         points3D_homogeneous = np.hstack((points3D[:, :3], np.ones((points3D.shape[0], 1))))
@@ -131,12 +126,13 @@ class SensorFusion(Node):
                         (points2D[:, 1] < w - 1))
         points2D = points2D[inrange[0]].round().astype('int')
         points3D = points3D[inrange[0]]
-        intensities = intensities[inrange[0]]
         if is_realsense:
             segmentation_outputs, classes, conf = self.model_operator.predict(self.realsense_image_msg, CV_BRIDGE)
         else:
             segmentation_outputs, classes, conf = self.model_operator.predict(self.logitech_image_msg, CV_BRIDGE)
 
+        x_coordinates = []
+        y_coordinates = []
         median_distances = []
         center_points = []
         angles = []
@@ -149,111 +145,76 @@ class SensorFusion(Node):
                     centroid = np.average(vertices, axis=0)
                     vertices = np.array([centroid + 0.9 * (v-centroid) for v in vertices], np.int32)
                     cv2.fillPoly(mask, [vertices], 1)
-                    #cv2.fillPoly(mask, [np.array(segmentation_output, np.int32)], 1)
                     x_coords = points2D[:, 0]
                     y_coords = points2D[:, 1]
+
                     in_polygon = mask[y_coords, x_coords] == 1 
                     points_in_polygon = points3D[in_polygon]
+                    x_in_polygon = points_in_polygon[:, 0]
+                    y_in_polygon = points_in_polygon[:, 1]
+
                     if len(points_in_polygon) > 0:
-                        angle = np.arctan2(np.median(points_in_polygon[:, 0]), np.median(points_in_polygon[:, 1]))
-                        if angle < 0:
-                            angle = -((np.pi / 2) + angle)
-                        else:
-                            angle = (np.pi / 2) - angle
-                        angles.append(angle)
-                        distances = np.linalg.norm(points_in_polygon[:, :2], axis=1)
-                        median_distance = np.median(distances) if len(distances) > 0 else float('nan')
-                        median_distances.append(median_distance)
+                        median_x = np.median(x_in_polygon)
+                        median_y = np.median(y_in_polygon)
                         included_classes.append(classes[idx])
+                        x_coordinates.append(median_x)
+                        y_coordinates.append(median_y)
+
         #orange = 0, yellow = 1, blue = 2 in feb system
         classesToActual = {0: 0, 1: 1, 2: 7, 3: 8, 4: 9}
         yolo_class_to_feb_class = {8: 2, 1: 1, 0: 0, 7: 0, 9: 0}
         mask_conf = [idx for idx, con in enumerate(conf) if con > 0.7]
         chosen_classes = []
         chosen_classes = [yolo_class_to_feb_class[classesToActual[int(included_classes[i])]] for i in range(len(included_classes))]
-        return median_distances, angles, chosen_classes
+        return x_coordinates, y_coordinates, chosen_classes
 
     def project_both_point_clouds(self):
-        dists_realsense, angles_realsense_unmodified, classes_realsense = self.get_dist_angle_classes(self.velodyne_msg, self.realsense_image_msg, self.model_operator, True)
-        dists_logitech, angles_logitech_unmodified, classes_logitech = self.get_dist_angle_classes(self.velodyne_msg, self.logitech_image_msg, self.model_operator, False)
+        x_coords_realsense, y_coords_realsense, classes_realsense = self.get_dist_angle_classes(self.velodyne_msg, self.realsense_image_msg, self.model_operator, True)
+        x_coords_logitech, y_coords_logitech, classes_logitech = self.get_dist_angle_classes(self.velodyne_msg, self.logitech_image_msg, self.model_operator, False)
 
-        angles_logitech = [angle for angle in angles_logitech_unmodified]
-        angles_realsense = [angle for angle in angles_realsense_unmodified]
-        cones_msg = Cones()
-        cones_msg.header.stamp = self.get_clock().now().to_msg()
-
-        print("LOGITECH STUFF")
-        print("CONES R: ", dists_logitech)
-        print("CONES THETA: ", angles_logitech)
-        print("COLOR: ", classes_logitech)
-        print("==================================")
-        print("REALSENSE STUFF")
-        print("CONES R: ", dists_realsense)
-        print("CONES THETA: ", angles_realsense)
-        print("COLOR: ", classes_realsense)
-        print("================================================")
-        print("================================================")
-        
-        cartesian_x_distances_logitech = dists_logitech * np.cos(angles_logitech)
-        cartesian_y_distances_logitech = dists_logitech * np.sin(angles_logitech)
-        cartesian_x_distances_realsense = dists_realsense * np.cos(angles_realsense)
-        cartesian_y_distances_realsense = dists_realsense * np.sin(angles_realsense)
-
-        # Combine Cartesian coordinates and colors from both cameras
-        x_combined = np.concatenate([cartesian_x_distances_logitech, cartesian_x_distances_realsense])
-        y_combined = np.concatenate([cartesian_y_distances_logitech, cartesian_y_distances_realsense])
-        colors_combined = np.concatenate([classes_logitech, classes_realsense])
+      
+       # Combine Cartesian coordinates and colors from both cameras
+        x_combined = np.concatenate([x_coords_realsense, x_coords_logitech])
+        y_combined = np.concatenate([y_coords_realsense, y_coords_logitech])
+        colors_combined = np.concatenate([classes_realsense, classes_logitech])
 
         # Create a 2D array of points with colors
         points = np.column_stack((x_combined, y_combined, colors_combined))
         if len(points) == 0:
             return
-        
 
-        # Convert filtered Cartesian coordinates back to polar
-        filtered_r = np.sqrt(filtered_x**2 + filtered_y**2).astype(float)
-        filtered_r = [float(r) for r in filtered_r]
-        print(type(filtered_r[0]))
-        filtered_theta = np.arctan2(filtered_y, filtered_x)
-
-        filtered_theta =[float(theta) for theta in filtered_theta]
-        filtered_colors = [int(colors) for colors in filtered_colors]
-
-        cartesian_x_distances = filtered_r * np.cos(filtered_theta)
-        cartesian_y_distances = filtered_r * np.sin(filtered_theta)
-
-        cones_msg.r = filtered_r
-        cones_msg.theta = filtered_theta
-        cones_msg.color = filtered_colors
+        cones_msg = ConesCartesian()
+        cones_msg.header.stamp = self.get_clock().now().to_msg()
+        cones_msg.x = x_combined
+        cones_msg.y = y_combined
+        cones_msg.color = colors_combined
         self.perception_pub.publish(cones_msg)
 
         cones_guess = PointCloud()
         positions = []
-        for x, y, color_value in zip(cartesian_x_distances, cartesian_y_distances, filtered_colors):
+        for x, y, color_value in zip(x_combined, y_combined, colors_combined):
             positions.append(Point32())
             positions[-1].x = x
             positions[-1].y = y
             positions[-1].z = 0.0
+
         cones_guess.points = positions
-        cones_guess.header.frame_id = "map"
+        cones_guess.header.frame_id = "rslidar"
         cones_guess.header.stamp = self.get_clock().now().to_msg()
         self.perception_visual_pub.publish(cones_guess)
 
     def project_point_cloud(self):
         if self.logitech_image_msg is None:
             return
-        median_distances, angles, chosen_classes = self.get_dist_angle_classes(is_realsense=False)
+        x_coords, y_coords, chosen_classes = self.get_dist_angle_classes(is_realsense=False)
 
         cones_msg = Cones()
         cones_msg.header.stamp = self.get_clock().now().to_msg()
 
-        cartesian_x_distances = median_distances * np.cos(angles)
-        cartesian_y_distances = median_distances * np.sin(angles)
-
         cones_guess = PointCloud()
         cones_cartesian = ConesCartesian()
         positions = []
-        for x, y, color_value in zip(cartesian_x_distances, cartesian_y_distances, chosen_classes):
+        for x, y, color_value in zip(x_coords, y_coords, chosen_classes):
             positions.append(Point32())
             positions[-1].x = x
             positions[-1].y = y

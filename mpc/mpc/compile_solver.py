cc512708c220d379d40b979f37359d89e1fbbd4a
Daniel Municio <danielmunicio360@gmail.com>
2025-05-16 15:26:01 -0700

    Removing non-MWEKF files

--- mpc/mpc/compile_solver.py
+++
@@ -1,9 +0,0 @@
-#!python
-from .mpc_controller import MPCPathFollower
-from all_settings.all_settings import MPCSettings as settings
-
-def build_solver():
-    MPCPathFollower(**settings).construct_solver(generate_c=True, compile_c=True, gcc_opt_flag='-Ofast')
-
-if __name__ == '__main__':
-    build_solver()
--- sensor_fusion/Extrinsic_Camera_Calibration/src/lidar_camera_calibration/utilities/test.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-extrinsics = np.load('/home/dhruvagarwal/feb-system-integration/sensor_fusion/Extrinsic_Camera_Calibration/src/lidar_camera_calibration/utilities/extrinsics.npz')
-print(extrinsics['R'])
-print(extrinsics['T'])
